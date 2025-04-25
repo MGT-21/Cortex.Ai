5c8b553754bfc54acd09b2b02bd326afeb3b654b
Marcello Augusto <139600086+MGT-21@users.noreply.github.com>
2025-04-24 23:40:42 -0300

    Upload dos arquivos base

---
+++ scripts/index.js
@@ -0,0 +1,12 @@
+VANTA.TRUNK({
+    el: "#animatedBackground",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 300.00,
+    minWidth: 300.00,
+    scale: 1.00,
+    spacing: 1.50,
+    scaleMobile: 1.00,
+    chaos: 2.00
+  })
